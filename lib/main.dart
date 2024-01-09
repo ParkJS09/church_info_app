f1d2c87165bac0704260f041299d01ee978768af
parkjs <python2_7@naver.com>
2024-01-10 00:18:35 +0900

    공통으로 사용하기 위한 share package 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:church_info_app/src/service/theme_service.dart';
+import 'package:church_info_app/share/service/theme_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
